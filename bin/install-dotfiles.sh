016399b6477d05bbb593a7b842a17529e78245f4
Jonas Juselius <jonas.juselius@uit.no>
2015-02-28 13:28:39 +0100

    Modular installer.

--- bin/install-dotfiles.sh
+++ bin/install-dotfiles.sh
@@ -2,14 +2,9 @@
 
 dt=`date +%Y%m%d-%H%M`
 
-cd $HOME
+cd ~
 [ ! -d ~/.dotfiles.$dt ] && mkdir ~/.dotfiles.$dt
 [ ! -d ~/.dotfiles.$dt/.ssh ] && mkdir ~/.dotfiles.$dt/.ssh
-
-git submodule init
-git submodule update
-
-(cd ~/.dotfiles/spf13-vim; ./bootstrap.sh)
 
 read -r -d '' dots << EOF
 .aliases
@@ -29,27 +24,51 @@ read -r -d '' dots << EOF
 .zshrc
 EOF
 
-for i in $dots; do
-    [ -e $i ] && mv $i .dotfiles.$dt
-done
+install_submodules () {
+    cd ~/.dotfiles
+    git submodule init
+    git submodule update
+    cd ~/.dotfiles/spf13-vim
+    ./bootstrap.sh
+}
 
 for i in $dots; do
     ln -s .dotfiles/$i .
 done
 
-if [ -d ~/bin ]; then
-    cd ~/bin
-    ln -s ~/.dotfiles/bin/{git-hscope,haskell-install.sh} .
-    cd -
-fi
+backup_dotfiles () {
+    for i in $dots; do
+        [ -e $i ] && mv $i .dotfiles.$dt
+    done
+}
 
-# configure bash
-cat << EOF >> ~/.bashrc
+install_dotfiles () {
+    for i in $dots; do
+        ln -s .dotfiles/$i .
+    done
+}
 
-shopt -s extglob
+install_haskell_extras () {
+    if [ -d ~/bin ]; then
+        cd ~/bin
+        ln -s ~/.dotfiles/bin/{git-hscope,haskell-install.sh} .
+    fi
+}
 
-GIT_PROMPT_ONLY_IN_REPO=1
-. ~/.dotfiles/bash-git-prompt/gitprompt.sh
+configure_bash () {
+    cat << EOF >> ~/.bashrc
+
+    shopt -s extglob
+
+    GIT_PROMPT_ONLY_IN_REPO=1
+    . ~/.dotfiles/bash-git-prompt/gitprompt.sh
 
 EOF
+}
+
+(backup_dotfiles)
+(install_submodules)
+(install_dotfiles)
+(install_haskell_extras)
+(configure_bash)
 
